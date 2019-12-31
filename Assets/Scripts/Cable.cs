3ecfb2ed26bf8f612e1c12ed0ebc1bc70d2dc573
Yigitcan Alanyali <y.alanyali@gmail.com>
2019-12-31 16:02:40 +0100

    REFACTOR

--- Assets/Scripts/Cable.cs
+++ Assets/Scripts/Cable.cs
@@ -4,89 +4,79 @@ using UnityEngine;
 
 public class Cable : MonoBehaviour
 {
-    [SerializeField] private Transform StartPoint;
+    [SerializeField] protected Transform m_StartPoint;
 
-    [SerializeField] private Transform EndPoint;
+    [SerializeField] protected Transform m_EndPoint;
 
-    private LineRenderer lineRenderer;
-    private List<CableSegment> cableSegments = new List<CableSegment>();
-    public float cableSegLen = 0.1f; // 0.25f;
-    public int segmentLength = 20;
-    public float lineWidth = 0.1f;
+    protected LineRenderer m_LineRenderer;
+    protected List<CableSegment> m_CableSegments = new List<CableSegment>();
+    public float m_CableSegmentLength = 0.1f; // 0.25f;
+    public int m_CableSegmentCount = 20;
+    public float m_LineWidth = 0.1f;
 
-    // Use this for initialization
     void Start()
     {
-        this.lineRenderer = this.GetComponent<LineRenderer>();
-        Vector3 cableStartPoint = StartPoint.position;
+        m_LineRenderer = GetComponent<LineRenderer>();
+        Vector3 cableStartPoint = m_StartPoint.position;
 
-        for (int i = 0; i < segmentLength; i++)
+        for (int i = 0; i < m_CableSegmentCount; i++)
         {
-            this.cableSegments.Add(new CableSegment(cableStartPoint));
-            cableStartPoint.y -= cableSegLen;
+            m_CableSegments.Add(new CableSegment(cableStartPoint));
+            cableStartPoint.y -= m_CableSegmentLength;
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        this.DrawCable();
-    }
+    void Update() => DrawCable();
 
-    private void FixedUpdate()
-    {
-        this.Simulate();
-    }
+    void FixedUpdate() => Simulate();
 
-    private void Simulate()
+    protected void Simulate()
     {
-        // SIMULATION
         Vector2 forceGravity = new Vector2(0f, -1f);
 
-        for (int i = 1; i < this.segmentLength; i++)
+        for (int i = 1; i < m_CableSegmentCount; i++)
         {
-            CableSegment firstSegment = this.cableSegments[i];
+            CableSegment firstSegment = m_CableSegments[i];
             Vector2 velocity = firstSegment.posNow - firstSegment.posOld;
             firstSegment.posOld = firstSegment.posNow;
             firstSegment.posNow += velocity;
             firstSegment.posNow += forceGravity * Time.fixedDeltaTime;
-            this.cableSegments[i] = firstSegment;
+            m_CableSegments[i] = firstSegment;
         }
 
-        //CONSTRAINTS
         for (int i = 0; i < 50; i++)
         {
-            this.ApplyConstraint();
+            ApplyConstraint();
         }
     }
 
-    private void ApplyConstraint()
+    protected void ApplyConstraint()
     {
-        //Constrant to First Point 
-        CableSegment firstSegment = this.cableSegments[0];
-        firstSegment.posNow = this.StartPoint.position;
-        this.cableSegments[0] = firstSegment;
+        //Constraint to First Point 
+        CableSegment firstSegment = m_CableSegments[0];
+        firstSegment.posNow = m_StartPoint.position;
+        m_CableSegments[0] = firstSegment;
 
 
-        //Constrant to Second Point 
-        CableSegment endSegment = this.cableSegments[this.cableSegments.Count - 1];
-        endSegment.posNow = this.EndPoint.position;
-        this.cableSegments[this.cableSegments.Count - 1] = endSegment;
+        //Constraint to Second Point 
+        CableSegment endSegment = m_CableSegments[m_CableSegments.Count - 1];
+        endSegment.posNow = m_EndPoint.position;
+        m_CableSegments[m_CableSegments.Count - 1] = endSegment;
 
-        for (int i = 0; i < this.segmentLength - 1; i++)
+        for (int i = 0; i < m_CableSegmentCount - 1; i++)
         {
-            CableSegment firstSeg = this.cableSegments[i];
-            CableSegment secondSeg = this.cableSegments[i + 1];
+            CableSegment firstSeg = m_CableSegments[i];
+            CableSegment secondSeg = m_CableSegments[i + 1];
 
             float dist = (firstSeg.posNow - secondSeg.posNow).magnitude;
-            float error = Mathf.Abs(dist - this.cableSegLen);
+            float error = Mathf.Abs(dist - m_CableSegmentLength);
             Vector2 changeDir = Vector2.zero;
 
-            if (dist > cableSegLen)
+            if (dist > m_CableSegmentLength)
             {
                 changeDir = (firstSeg.posNow - secondSeg.posNow).normalized;
             }
-            else if (dist < cableSegLen)
+            else if (dist < m_CableSegmentLength)
             {
                 changeDir = (secondSeg.posNow - firstSeg.posNow).normalized;
             }
@@ -95,40 +85,40 @@ public class Cable : MonoBehaviour
             if (i != 0)
             {
                 firstSeg.posNow -= changeAmount * 0.5f;
-                this.cableSegments[i] = firstSeg;
+                m_CableSegments[i] = firstSeg;
                 secondSeg.posNow += changeAmount * 0.5f;
-                this.cableSegments[i + 1] = secondSeg;
+                m_CableSegments[i + 1] = secondSeg;
             }
             else
             {
                 secondSeg.posNow += changeAmount;
-                this.cableSegments[i + 1] = secondSeg;
+                m_CableSegments[i + 1] = secondSeg;
             }
         }
     }
 
-    private void DrawCable()
+    protected void DrawCable()
     {
-        float lineWidth = this.lineWidth;
-        lineRenderer.startWidth = lineWidth;
-        lineRenderer.endWidth = lineWidth;
+        // float m_LineWidth = m_LineWidth;
+        m_LineRenderer.startWidth = m_LineWidth;
+        m_LineRenderer.endWidth = m_LineWidth;
 
-        Vector3[] cablePositions = new Vector3[this.segmentLength];
-        for (int i = 0; i < this.segmentLength; i++)
+        Vector3[] cablePositions = new Vector3[m_CableSegmentCount];
+        for (int i = 0; i < m_CableSegmentCount; i++)
         {
-            cablePositions[i] = this.cableSegments[i].posNow;
+            cablePositions[i] = m_CableSegments[i].posNow;
         }
 
-        lineRenderer.positionCount = cablePositions.Length;
-        lineRenderer.SetPositions(cablePositions);
+        m_LineRenderer.positionCount = cablePositions.Length;
+        m_LineRenderer.SetPositions(cablePositions);
     }
 
-    public float GetTailAngle()
-    {
-        CableSegment p1 = this.cableSegments[this.cableSegments.Count - 1];
-        CableSegment p2 = this.cableSegments[this.cableSegments.Count - 2];
-        return Mathf.Atan2(p2.posNow.y - p1.posNow.y, p2.posNow.x - p1.posNow.x) * Mathf.Rad2Deg;
-    }
+    // public float GetTailAngle()
+    // {
+    //     CableSegment p1 = m_CableSegments[m_CableSegments.Count - 1];
+    //     CableSegment p2 = m_CableSegments[m_CableSegments.Count - 2];
+    //     return Mathf.Atan2(p2.posNow.y - p1.posNow.y, p2.posNow.x - p1.posNow.x) * Mathf.Rad2Deg;
+    // }
 
     public struct CableSegment
     {
@@ -137,8 +127,8 @@ public class Cable : MonoBehaviour
 
         public CableSegment(Vector2 pos)
         {
-            this.posNow = pos;
-            this.posOld = pos;
+            posNow = pos;
+            posOld = pos;
         }
     }
 }
